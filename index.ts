ba5c55fc44460a8ddb665976fd4e9971a9702104
ArtiolaDomiCaka <artioladomi@hotmail.com>
2022-03-04 14:22:30 +0100

    more

--- index.ts
+++ index.ts
@@ -9,18 +9,65 @@ app.use(express.json())
 const db = new Database('./data.db', {
     verbose: console.log
 })
-const getUsers = db.prepare(`
-SELECT * FROM users;
+//Welcome message
+app.get('/', (req, res) => {
+    res.send(` <h1>Welcome to Reddit API</h1>
+    <p>Here are all the <b>endpoints</b> you need to work with.. </p>
+    `)
+})
+
+//User endpoint
+const createUser = db.prepare(`
+INSERT INTO users(name, email, password, displayName) VALUES (?, ?, ?, ?);
 `)
 
-const createUsers = db.prepare(`
-INSERT INTO users(name, email, password, displayName) VALUES (?, ?, ?, ?);
+const getUsers = db.prepare(`
+SELECT * FROM users;
 `)
 
 const getUserById = db.prepare(`
 SELECT * FROM users WHERE id=?;
 `)
 
+// app.get('/users')
+// app.get('/posts')
+// app.post('/posts')
+// app.delete('/posts/:id')
+// app.get('/subreddits')
+
+app.post('/sign-in', (req, res ) => {
+    // to sign in a user needs to send us their email and password
+    const {email, password} = req.body
+    // check if the email exists
+  // check if the password matches
+  // give them back the user if they match
+  // give them back an error if they don't
+  res.send({ error: 'Noop'})
+})
+
+app.post('/users', (req, res ) => {
+    const{ name, email, password, displayName } = req.body
+    const info = createUser.run(name, email, password, displayName)
+    const user = getUserById.get(info.lastInsertRowid)
+    res.send(user)
+})
+
+// const createSubreddit = db.prepare(`
+// INSERT INTO subreddit(description, background) VALUES (?, ?);
+// `)
+// const getSubreddits = db.prepare(`
+// SELECT * FROM subreddits;
+// `)
+
+// const getSubredditById = db.prepare(`
+// SELECT * FROM subreddits WHERE id=?;
+// `)
+// app.post('/subreddits', (req, res) => {
+//     const { description, background} = req.body
+//     const info = createSubreddit.run(description , background)
+//     const subreddit = getSubredditById.get(info.lastInsertRowid)
+//     res.send(subreddit)
+// })
 // app.get('/post', (req, res) => {
 //  const users = getUsers.all()
 //  for(const user of users){
